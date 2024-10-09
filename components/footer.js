b90490b477cb5d04ab3c6d79a9b8ea735fa48b90
RuslanFom <ruslanfom@gmail.com>
2024-10-09 18:46:42 +0200

    fix footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,18 @@
-import { Box, Text, Container } from '@chakra-ui/react'
+import { Box, Text, Container, useColorModeValue } from '@chakra-ui/react'
 
 export default function Footer() {
   return (
-    <Container m={0} p={10} w="full" h="full" maxW="full" bg="gray.800" borderBottomRadius="xl">
+    <Container
+      m={0}
+      p={10}
+      w="full"
+      h="full"
+      maxW="full"
+      bg={useColorModeValue(' #E2E4E4', 'gray.800')}
+      borderBottomRadius="xl"
+    >
       <Box align="center" opacity={0.4} fontSize="md">
-        <Text color="white">
+        <Text color={useColorModeValue('black', 'white')}>
           &copy; {new Date().getFullYear()} CompanyMedics GmbH | Tel: 089–95 45
           86 920 | kontakt@companymedics.de
         </Text>
