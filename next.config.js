785f3eb7261fa5ac23804d21698fc575258419ab
RuslanFom <ruslanfom@gmail.com>
2024-10-23 15:17:48 +0200

    implement translation of navbar

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-    reactStrictMode: true,
-    i18n,
-};
+  reactStrictMode: true,
+  i18n,
+  publicRuntimeConfig: {
+    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+      ? process.env.LOCALE_SUBPATHS
+      : 'none',
+  },
+}
 
