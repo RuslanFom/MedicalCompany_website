7bcf59fb550db4fd6956cbd299ee6e2bbdbf1170
RuslanFom <ruslanfom@gmail.com>
2024-10-23 15:27:24 +0200

    fix bugs

--- i18n.js
+++ i18n.js
@@ -1,11 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
-import enTranslations from './public/locales/en/common.json'
-import itTranslations from './public/locales/it/common.json'
-import ruTranslations from './public/locales/ru/common.json'
-import uaTranslations from './public/locales/ua/common.json'
-import deTranslations from './public/locales/de/common.json'
+
 
 i18n
 .use(Backend)
