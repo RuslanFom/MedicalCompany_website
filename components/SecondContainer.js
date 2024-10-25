6dcd5193520f76ae438f1e3a05d10bfc520322fe
RuslanFom <ruslanfom@gmail.com>
2024-10-25 16:09:08 +0200

    implement online-app

--- components/SecondContainer.js
+++ components/SecondContainer.js
@@ -1,4 +1,4 @@
-import { Container, Heading, Image, Text } from '@chakra-ui/react'
+import { Container, Heading, Image, Text, Box, Flex } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
 export const SecondContainer = () => (
@@ -7,92 +7,61 @@ export const SecondContainer = () => (
     gridTemplateColumns={{
       base: '1fr',
       md: '1fr 1fr',
-      lg: '1fr 1fr 1fr 1fr'
+      lg: 'repeat(4, minmax(0, 1fr))'
     }}
-    gap={0} // Уменьшаем расстояние между контейнерами
+    gap={{ base: 6, md: 4, lg: 2 }}
     mx="auto"
     my="70px"
     p={0}
-    maxW="100%"
+    maxW={{ base: "95%", md: "100%" }}
     justifyContent="center"
   >
-    <Container
-      w="274px"
-      h="212px"
-      display="flex"
-      flexDirection="column"
-      align="center"
-      justifyContent="center"
-    >
-      <NextLink href="/">
-        <Image src="/images/am_01-610268fb.webp" alt="betriebsmedizin" />
-        <Heading as="h2" fontSize="2xl" mt="20px">
-          Betriebsmedizin
-        </Heading>
-        <Text fontFamily="'Montserrat', sans-serif" mt="20px">
-          Sämtliche arbeitsmedizinische Leistungen nach ASIG und DGUV V2
-        </Text>
-      </NextLink>
-    </Container>
-
-    <Container
-      w="274px"
-      h="212px"
-      display="flex"
-      flexDirection="column"
-      align="center"
-      justifyContent="center"
-    >
-      <NextLink href="/">
-        <Image src="/images/am_02.svg" alt="betriebsmedizin" />
-        <Heading as="h2" fontSize="2xl" mt="20px">
-          Arbeitsmedizinische Untersuchungen
-        </Heading>
-        <Text fontFamily="'Montserrat', sans-serif" mt="20px">
-          In unserer Praxis oder vor Ort in Ihrem Betrieb
-        </Text>
-      </NextLink>
-    </Container>
-
-    <Container
-      w="274px"
-      h="212px"
-      display="flex"
-      flexDirection="column"
-      align="center"
-      justifyContent="center"
-    >
-      <NextLink href="/">
-        <Image
-          src="/images/berufl_gf.svg"
-          alt="betriebsmedizin"
-        />
-        <Heading as="h2" fontSize="2xl" mt="20px">
-          Gesundheits-management
-        </Heading>
-        <Text fontFamily="'Montserrat', sans-serif" mt="20px">
-          Schritt für Schritt zur neuen Unternehmenskultur
-        </Text>
-      </NextLink>
-    </Container>
-
-    <Container
-      align="center"
-      w="274px"
-      h="212px"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-    >
-      <NextLink href="/">
-        <Image src="/images/as.svg" alt="betriebsmedizin" pb="17px" />
-        <Heading as="h2" fontSize="2xl" mb="45px" mt="10px">
-          Arbeitssicherheit
-        </Heading>
-        <Text fontFamily="'Montserrat', sans-serif" mt="10px" mb="7px">
-          Gefährdungsbeurteilungen und Ableitungen von Maßnahmen
-        </Text>
-      </NextLink>
-    </Container>
+    {[
+      {
+        image: "/images/am_01-610268fb.webp",
+        title: "Betriebsmedizin",
+        text: "Sämtliche arbeitsmedizinische Leistungen nach ASIG und DGUV V2"
+      },
+      {
+        image: "/images/am_02.svg",
+        title: "Arbeitsmedizinische Untersuchungen",
+        text: "In unserer Praxis oder vor Ort in Ihrem Betrieb"
+      },
+      {
+        image: "/images/berufl_gf.svg",
+        title: "Gesundheits-management",
+        text: "Schritt für Schritt zur neuen Unternehmenskultur"
+      },
+      {
+        image: "/images/as.svg",
+        title: "Arbeitssicherheit",
+        text: "Gefährdungsbeurteilungen und Ableitungen von Maßnahmen"
+      }
+    ].map((item, index) => (
+        <Flex
+        key={index}
+        w="100%"
+        maxW="274px"
+        h={{ base: "auto", lg: "212px" }}
+        flexDirection="column"
+        align="center"
+        justifyContent="center"
+        mx="auto"
+      >
+        <NextLink href="/" passHref>
+          <Box as="a" textAlign="center" w="100%">
+            <Flex justify="center" mb="20px">
+              <Image src={item.image} alt={item.title} />
+            </Flex>
+            <Heading as="h2" fontSize="2xl" mb="20px">
+              {item.title}
+            </Heading>
+            <Text fontFamily="'Montserrat', sans-serif">
+              {item.text}
+            </Text>
+          </Box>
+        </NextLink>
+      </Flex>
+    ))}
   </Container>
 )
--- pages/terminvereinbarung.js
+++ pages/terminvereinbarung.js
@@ -1,18 +1,48 @@
 import Layout from '../components/layouts/Article'
-import {Box, Button, Container, Image } from '@chakra-ui/react'
+import {Box, Button, Container, Image, Text } from '@chakra-ui/react'
 import NextLink from "next/link";
+import { useTranslation } from 'next-i18next';
 
-const Terminvereinbarung = () => (
+const Terminvereinbarung = () => {
+  const { t } = useTranslation('common')
+
+  return (
   <Layout title='terminvereinbarung'>
-    <Container>
-      <Image src="/images/leistungen_am-98f373aa.webp" alt="leistungen" />
+    <Container
+      display="flex"
+      flexDirection="column"
+      maxW="full" // Контейнер занимает всю ширину
+      w="full"
+      px={0}
+      mt={5}
+    >
+      <Image 
+      src="/images/praxis_head-daf80ec8.webp" 
+      alt="leistungen" 
+      h={{ base: "166.38px", md: "123.19px", lg: "248px", xl: "194px" }}
+      objectFit="cover"
+      />
+    </Container>
+    <Container py="70px" m={0} display="flex" justifyContent="center" alignItems="center"> 
+      <Container m={0} p={0}> 
+        <Text as="h2" fontSize="md" py="15px"
+        px="25px">
+          Willkommen auf unserer Online-Terminbuchungsseite. 
+          In wenigen Schritten können Sie einen Termin bei uns online buchen. 
+          Wir freuen uns auf Sie!
+        </Text>
+      </Container>
     </Container>
     <Box my={6} align="center">
-      <NextLink href="/">
-        <Button bgGradient="linear(to-r, #3cd3ad, #6dd5fa)">Return to home</Button>
+      <NextLink href="/" passHref>
+        <Button as="a" bgGradient="linear(to-r, #3cd3ad, #6dd5fa)">
+          {t('terminvereinbarung.button')}
+        </Button>
       </NextLink>
     </Box>
   </Layout>
 )
+}
 
 export default Terminvereinbarung
+export {getServerSideProps} from '../pages/index'
